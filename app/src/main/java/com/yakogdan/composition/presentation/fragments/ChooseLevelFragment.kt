dfeb3f321fa358ff52bc3d248d519d8fadc236da
yakogdan <yakogdan@gmail.com>
2023-07-08 15:19:02 +0800

    Добавлены нужные фрагменты и макеты к ним

---
+++ app/src/main/java/com/yakogdan/composition/presentation/fragments/ChooseLevelFragment.kt
@@ -0,0 +1,19 @@
+package com.yakogdan.composition.presentation.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.yakogdan.composition.R
+
+
+class ChooseLevelFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_choose_level, container, false)
+    }
+}

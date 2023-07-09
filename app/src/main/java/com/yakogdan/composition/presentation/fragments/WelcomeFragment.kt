1f12931ca819e77be4484d6a7c64f7fdaf101235
yakogdan <yakogdan@gmail.com>
2023-07-09 15:22:05 +0800

    Добавлен viewBinding

--- app/src/main/java/com/yakogdan/composition/presentation/fragments/WelcomeFragment.kt
+++ app/src/main/java/com/yakogdan/composition/presentation/fragments/WelcomeFragment.kt
@@ -5,15 +5,30 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import com.yakogdan.composition.R
-
+import com.yakogdan.composition.databinding.FragmentWelcomeBinding
 
 class WelcomeFragment : Fragment() {
 
+    private var _binding: FragmentWelcomeBinding? = null
+    private val binding: FragmentWelcomeBinding
+        get() = _binding ?: throw RuntimeException("FragmentWelcomeBinding == null")
+
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_welcome, container, false)
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentWelcomeBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.buttonUnderstand.setOnClickListener {
+
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
